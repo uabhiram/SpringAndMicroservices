168bac74c99f6dbc9e35fa803c013b778b074585
abhiramuppaluri <abhiramuppaluri@192.168.1.13>
2020-04-30 01:43:36 -0400

    first commit

---
+++ first-maven-project/src/main/java/org/springframework/first_maven_project/App.java
@@ -0,0 +1,13 @@
+package org.springframework.first_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
